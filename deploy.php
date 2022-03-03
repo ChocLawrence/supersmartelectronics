9a388e179bf3f3b13ed3b03632908b37567cb64b
Lawrence Elango <lawrence@Lawrences-MacBook-Pro.local>
2022-03-03 14:53:31 +0100

    initial deploy deployment

---
+++ deploy.php
@@ -0,0 +1,44 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'sselectronics');
+
+// Project repository
+set('repository', 'git@github.com:ChocLawrence/sselectronics.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+//set('use_relative_symlinks', false);
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('foodstability.com')
+    ->user('lawrence')
+    ->identityFile('~/.ssh/sselectronics_deployerkey')
+    ->set('deploy_path', '/var/www/html/sselectronics');    
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
